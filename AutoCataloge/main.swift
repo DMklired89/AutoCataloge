d1c56ef38755e2e003e0d91f1f152279c7b5b22b
Dmitry Edrenov <edrenoff@icloud.com>
2019-03-30 14:08:45 +0700

    Initial Commit

---
+++ AutoCataloge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AutoCataloge
+//
+//  Created by Dmitry on 30/03/2019.
+//  Copyright © 2019 Dmitry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
